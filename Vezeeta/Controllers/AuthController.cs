d1a28f1256618eb5323777973b92aa010e7b4d7e
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2025-08-18 10:39:58 +0300

    Add Controllers

---
+++ Vezeeta/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿namespace Vezeeta.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthController : BaseApiController
+{
+    [HttpPost("register")]
+
+    public async Task<IActionResult> Register([FromForm] Features.Authentication.Commands.Register.RegisterRequest request)
+    {
+        var result = await Mediator.Send(request);
+        return result is null ? BadRequest("Registration failed. Please try again.") : Ok(result);
+    }
+
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync(Features.Authentication.Queries.Login.LoginRequest request)
+    {
+        var authResult = await Mediator.Send(request);
+        return authResult is null ? BadRequest("Invalid Email/Password") : Ok(authResult);
+    }
+}
---
+++ Vezeeta/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vezeeta.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseApiController : ControllerBase
+{
+    protected ISender Mediator => HttpContext.RequestServices.GetRequiredService<ISender>();
+}
