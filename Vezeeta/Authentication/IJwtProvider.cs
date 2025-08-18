580c36b54b23aedfe0bb0d376307958a20aef9f1
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2025-08-18 10:39:20 +0300

    Add Authentication

---
+++ Vezeeta/Authentication/IJwtProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Vezeeta.Authentication;
+
+public interface IJwtProvider
+{
+    (string token, int expiresIn) GenerateToken(ApplicationUser user);
+}
---
+++ Vezeeta/Authentication/JwtProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Vezeeta.Authentication;
+
+public class JwtProvider(IConfiguration configuration) : IJwtProvider
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public (string token, int expiresIn) GenerateToken(ApplicationUser user)
+    {
+        
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
+            new Claim(JwtRegisteredClaimNames.Birthdate, user.DateOfBirth.ToString("yyyy-MM-dd")),
+            new Claim(JwtRegisteredClaimNames.Gender, user.Gender.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+        var expiresInMinutes = int.Parse(_configuration["Jwt:ExpiryMinutes"]!);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(expiresInMinutes),
+            signingCredentials: signingCredentials
+            );
+
+        return ( token: new JwtSecurityTokenHandler().WriteToken(token), expiresIn: expiresInMinutes * 60);
+    }
+}
