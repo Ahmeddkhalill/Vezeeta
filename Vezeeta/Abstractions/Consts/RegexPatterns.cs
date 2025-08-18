b02d4aab8870a656b72d9171d3e786d427af552e
Ahmed Khalil <136190754+Ahmeddkhalill@users.noreply.github.com>
2025-08-18 10:39:46 +0300

    Add RegexPattern for Validations

---
+++ Vezeeta/Abstractions/Consts/RegexPatterns.cs
@@ -0,0 +1,7 @@
+﻿namespace Vezeeta.Abstractions.Consts;
+
+public static class RegexPatterns
+{
+    public const string Password = "(?=(.*[0-9]))(?=.*[\\!@#$%^&*()\\\\[\\]{}\\-_+=~`|:;\"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}";
+    public const string PhoneNumber = @"^\d{10,11}$";
+}
